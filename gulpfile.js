88ffae92c8f5db873889a31d25829c316a501ad1
Aditya Jain <aditya.jain.im@gmail.com>
2017-12-16 22:48:08 +0530

    new commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    spawn = require('child_process').spawn,
+    node;
+
+gulp.task('server', function() {
+  if (node) node.kill()
+  node = spawn('node', ['server.js'], {stdio: 'inherit'})
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+})
+gulp.task('default', function() {
+  gulp.run('server')
+
+  gulp.watch(['./server.js', './public/libs/**/*.js','./public/js/**/*.js','./public/views/*.html','./public/views/**/*.html','./public/css/*.css'], function() {
+    gulp.run('server')
+  })
+})
+
+process.on('exit', function() {
+    if (node) node.kill()
+})
